83d5aa5f55fbc9a6e88dc63c6e3ae68f8e7b512a
植木善弘 <pirori67@gmail.com>
2014-09-29 23:40:01 +0900

    緯度経度から距離を計算途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
+
+#  def distance_calculation
+#    //緯度と経度から距離計算 a:基準、b:現在地//緯度と経度から距離計算 a:基準、b:現在地
+#    b_latitude = @users.last.latitude
+#    b_longitude = @users.last.longitude
+#  end
 end
