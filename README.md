a11b1240fdd004a7ae3c5bce7bb13546cdc6f91c
yoshixmk <pirori67@gmail.com>
2021-01-28 23:21:45 +0900

    setup pg

---
+++ README.md
@@ -0,0 +1,21 @@
+# yomi
+
+## Setup Database
+
+Composing postgres server and pgAdmin
+
+```bash
+$ docker-compose up
+```
+
+
+
+## Run
+
+Installing pg libs.
+
+```bash
+$ brew install postgresql
+
+$ bundle install
+```
---
+++ pg.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+# Output a table of current connections to the DB
+conn = PG.connect( dbname: 'yomi', host: 'localhost', user:'admin', password:'password' )
+conn.exec( "SELECT * FROM yomi.yamas LIMIT 100" ) do |result|
+  puts "          message | hash"
+  result.each do |row|
+    puts " %16s | %-16s " %
+      row.values_at('message', 'hash',)
+  end
+end
