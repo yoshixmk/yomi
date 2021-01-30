f472751e48ffa985651b59418564ff9d3a2ce7bc
yoshixmk <pirori67@gmail.com>
2021-01-30 23:58:41 +0900

    insert

--- pg.rb
+++ pg.rb
@@ -2,6 +2,9 @@ require 'pg'
 
 # Output a table of current connections to the DB
 conn = PG.connect( dbname: 'yomi', host: 'localhost', user:'admin', password:'password' )
+
+conn.exec( "INSERT INTO yomi.yamas values ( 'test', MD5('hashed'));" )
+
 conn.exec( "SELECT * FROM yomi.yamas LIMIT 100" ) do |result|
   puts "          message | hash"
   result.each do |row|
