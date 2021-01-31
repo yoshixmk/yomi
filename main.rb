f195720f2fdc6c1eaa1357c61b1669217a37f816
yoshixmk <pirori67@gmail.com>
2021-02-01 00:06:57 +0900

    bulk insert

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 require "./zpai"
 require "./dpai"
 require "./npai"
+require "./pg"
 
 all_pai = npai() + zpai() + dpai()
 
-p all_pai
+conn = open()
 
-p all_pai.size
+# wait ctrl + d
+while(!STDIN.eof) do
+  shuffled = 1000.times.map{|i| all_pai.shuffle.take(83).join()}
 
-p all_pai.shuffle.take(83)
+  insert(shuffled, conn)
+end
+
+close(conn)
--- pg.rb
+++ pg.rb
@@ -1,13 +1,23 @@
 require 'pg'
 
-conn = PG.connect( dbname: 'yomi', host: 'localhost', user:'admin', password:'password' )
+def open
+  conn = PG.connect( dbname: 'yomi', host: 'localhost', user:'admin', password:'password' )
+  return conn
+end
 
-conn.exec( "INSERT INTO yomi.yamas values ( 'test', MD5('hashed')) ON CONFLICT DO NOTHING;" )
+def insert(pais, conn)
+  values = pais.map{|pai| "('#{pai}', MD5('#{pai}'))"}.join(",")
+  conn.exec("INSERT INTO yomi.yamas values #{values} ON CONFLICT DO NOTHING;")
 
-conn.exec( "SELECT * FROM yomi.yamas LIMIT 100" ) do |result|
-  puts "          message | hash"
-  result.each do |row|
-    puts " %16s | %-16s " %
-      row.values_at('message', 'hash',)
-  end
+  # conn.exec( "SELECT * FROM yomi.yamas LIMIT 100" ) do |result|
+  #   puts "          message | hash"
+  #   result.each do |row|
+  #     puts " %16s | %-16s " %
+  #       row.values_at('message', 'hash',)
+  #   end
+  # end
+end
+
+def close(conn)
+  return conn.close()
 end
