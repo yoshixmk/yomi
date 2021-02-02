2608c5768e233db750669604a43eb3909a451b2b
yoshixmk <pirori67@gmail.com>
2021-02-02 10:06:13 +0900

    fix

--- npai.rb
+++ npai.rb
@@ -4,10 +4,10 @@ def npai
   category = ["m", "p", "s"]
 
   npai = category.flat_map{ |c|
-    nums.map { |n| c + n }
+    nums.map { |n| n + c }
   }
 
-  not_wupai_nums = npai.reject{ |e| ["m5", "p5", "s5"].include?(e)}
+  not_wupai_nums = npai.reject{ |e| ["5m", "5p", "5s"].include?(e)}
 
   npai * 3 + not_wupai_nums
 end
